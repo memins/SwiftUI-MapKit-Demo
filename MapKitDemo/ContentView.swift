c7eeddbdae6d7fcb2896093839b5c5d21d4b4ad2
Emin Sahin <m.emins@yahoo.com>
2023-07-18 23:17:39 +0200

    try the new MapKit

--- MapKitDemo/ContentView.swift
+++ MapKitDemo/ContentView.swift
@@ -6,19 +6,149 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    let uniWien = CLLocationCoordinate2D(
+        latitude: 48.213349904351425,
+        longitude: 16.360093313277204
+    )
+    
+    let votivKino = CLLocationCoordinate2D(
+        latitude: 48.21696729898015,
+        longitude: 16.3604902800422
+    )
+    
+    let parlament = CLLocationCoordinate2D(
+        latitude: 48.20820218289815,
+        longitude: 16.35977144851028
+    )
+    
+    @State private var camera: MapCameraPosition = .automatic
+    @State private var selectedLocation: CLLocationCoordinate2D?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Map(
+            position: $camera
+        ) {
+            Marker(
+                "Uni Wien",
+                systemImage: "building.2",
+                coordinate: uniWien
+            )
+            Marker(
+                "Votiv Kino",
+                coordinate: votivKino
+            ).tint(
+                .purple
+            )
+            Marker(
+                "Parlament",
+                systemImage: "building.columns",
+                coordinate: parlament
+            )
         }
-        .padding()
+        .onAppear {
+            camera = .region(
+                MKCoordinateRegion(
+                    center: uniWien,
+                    latitudinalMeters: 200,
+                    longitudinalMeters: 200
+                )
+            )
+            selectedLocation = uniWien
+        }
+        .safeAreaInset(
+            edge: .bottom
+        ) {
+            HStack {
+                Spacer(
+                )
+                
+                Button(action: {
+                    withAnimation {
+                        camera = .region(
+                            MKCoordinateRegion(
+                                center: uniWien,
+                                latitudinalMeters: 200,
+                                longitudinalMeters: 200
+                            )
+                        )
+                        selectedLocation = uniWien
+                    }
+                }) {
+                    HStack {
+                        Image(
+                            systemName: "building.2"
+                        )
+                        Text(
+                            "Uni Wien"
+                        )
+                        .font(
+                            .headline
+                        )
+                    }
+                }
+                .foregroundColor(
+                    .white
+                )
+                .padding(
+                )
+                .background(
+                    Color.accentColor
+                )
+                .cornerRadius(
+                    10
+                )
+                Button(action: {
+                    withAnimation {
+                        camera = .region(
+                            MKCoordinateRegion(
+                                center: parlament,
+                                latitudinalMeters: 200,
+                                longitudinalMeters: 200
+                            )
+                        )
+                        selectedLocation = parlament
+                    }
+                }) {
+                    HStack {
+                        Image(
+                            systemName: "building.columns"
+                        )
+                        Text(
+                            "Parlament"
+                        )
+                        .font(
+                            .headline
+                        )
+                    }
+                }
+                .foregroundColor(
+                    .white
+                )
+                .padding(
+                )
+                .background(
+                    Color.accentColor
+                )
+                .cornerRadius(
+                    10
+                )
+                Spacer(
+                )
+            }
+            .padding(
+                .top
+            )
+            .background(
+                .thinMaterial
+            )
+        }
     }
 }
 
 #Preview {
-    ContentView()
+    ContentView(
+    )
 }
