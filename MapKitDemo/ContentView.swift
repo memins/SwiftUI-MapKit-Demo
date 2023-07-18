097f9768536e1099946f397e1139efecc05c1ea9
Emin Sahin <m.emins@yahoo.com>
2023-07-18 21:54:24 +0200

    Initial Commit

---
+++ MapKitDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MapKitDemo
+//
+//  Created by Emin Sahin on 18.07.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MapKitDemo/MapKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitDemoApp.swift
+//  MapKitDemo
+//
+//  Created by Emin Sahin on 18.07.23.
+//
+
+import SwiftUI
+
+@main
+struct MapKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
